5bc416070656997143c26dfb48303d4333b3bb1e
Alan Kregness <apkregness@gmail.com>
2017-11-01 00:15:19 -0500

    Added stuff in Label and BetDisplay

---
+++ src/components/PlayerGUI/Label.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Label extends React.Component {
+  render() {
+    return (
+      <p>THIS IS A LABEL</p>
+    )
+  };
+}
+
+export default Label
--- src/components/PlayerGUI/PlayerGUI.jsx
+++ src/components/PlayerGUI/PlayerGUI.jsx
@@ -12,8 +12,6 @@ class PlayerGUI extends React.Component {
     };
   }
   render() {
-
-
     return (
       <div>
         <Cup />
